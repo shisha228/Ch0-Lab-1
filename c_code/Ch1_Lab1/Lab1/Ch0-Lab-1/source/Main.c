307d21fdb356a6fdeacc8a013260d639d5e3d557
shisha228 <55587557+shisha228@users.noreply.github.com>
2019-10-04 20:53:16 +0800

    Add files via upload

---
+++ c_code/Ch1_Lab1/Lab1/Ch0-Lab-1/source/Main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+
+{
+
+	printf("Wellcome to C!\n");
+	system("pause");
+	return 0;
+}
---
+++ c_code/Ch1_Lab2/source/User_defined.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int  User_defined_print()
+{
+	printf("Welcome to C!\n");
+	system("pause");
+	return 0;
+
+}
